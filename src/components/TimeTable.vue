3369f90edbb67d32d62377b9e88fd5463c302350
ianisnotlan <ian990436@gmail.com>
2021-09-05 16:35:12 +0800

    revise

--- src/components/TimeTable.vue
+++ src/components/TimeTable.vue
@@ -4,18 +4,23 @@
     <div class="dropdown">
       <select v-model="selected">
         <option disabled>請選擇路線</option>
-        <option v-for="(item, index) in routeName" :key="index">{{item}}</option>
+        <option v-for="item in routeName" :key="item">{{item}}</option>
       </select>
       &nbsp;
       <button @click="onSearch()">查詢</button>
     </div>
-    <div v-if="routeInfo.length" class="container">
-      <div v-for="(info, index) in routeInfo" :key="index" class="each">
-        <p>路線： {{info.routeName}}</p>
-        <p>站名： {{info.stopName}}</p>
-        <p>{{info.estimateTime}}</p>
+    <template v-if="routeInfo.length">
+      <div v-for="info in routeInfo" :key="info" class="box">
+        <h2 class="name">路線： {{info.routeName}}</h2>
+        <div class="container">
+          <div v-for="(stop) in info.stop" :key="stop" class="each"> 
+            <p>站名： {{stop.name}}</p>
+            <p>預計到站時間：</p>
+            <p>{{stop.estimateTime}}</p>
+          </div>
+        </div>
       </div>
-    </div>
+    </template>
   </div>
 </template>
 
@@ -30,7 +35,7 @@ export default {
       routeName: [],
       selected: '請選擇路線',
       routeInfo: [],
-      previousUpdate: 0,
+      previousUpdate: false,
     }
   },
   watch: {
@@ -39,13 +44,19 @@ export default {
     }
   },
   mounted() {
-    let time = new Date()
-    this.previousUpdate = time.getTime()
     this.getBusData()
   },
   methods: {
+    timer() {
+      setTimeout(() => {
+        this.timeToUpdate = true
+        console.log('next search will update Bus data')
+      }, 60000)
+    },
     async getBusData() {
       this.busData = await getData()
+      this.timeToUpdate = false
+      this.timer()
       console.log('updateBusData')
     },
     getRouteName() {
@@ -56,33 +67,35 @@ export default {
     },
     async onSearch() {
       this.routeInfo = []
-      let time = new Date()
-      let currentTime = time.getTime()
       // update bus data only when last update was more than a minute ago
-      if (currentTime - this.previousUpdate > 60000) {
+      if (this.timeToUpdate) {
         await this.getBusData()
-        this.previousUpdate = currentTime
       }
 
       for (let each of this.busData) {
         if (each.RouteName.Zh_tw == this.selected) {
-          let info = {
-            routeName: '',
-            stopName: '',
-            estimateTime: '',
-          }
-          info.routeName = each.SubRouteName ? each.SubRouteName.Zh_tw : each.RouteName.Zh_tw
-          info.stopName = each.StopName.Zh_tw
+          let routeName = each.SubRouteName ? each.SubRouteName.Zh_tw : each.RouteName.Zh_tw
+          let stopName = each.StopName.Zh_tw
+          let estimateTime
 
           if (each.EstimateTime) {
             let time = parseInt(each.EstimateTime);
             let hours = Math.floor(time / 3600) == 0 ? '' : (Math.floor(time / 3600) + ' 時 ')
             let minutes = (Math.floor(time / 60) % 60) == 0 ? '' : ((Math.floor(time / 60) % 60) + ' 分 ')
             let seconds = time % 60 == 0 ? '' : (time % 60 + ' 秒')
-            info.estimateTime = `預計到站時間： ${hours}${minutes}${seconds}`
-          } else info.estimateTime = '預計到站時間：-'
+            estimateTime = hours + minutes + seconds
+          } else estimateTime = '-'
 
-          this.routeInfo.push(info)
+          let found = this.routeInfo.findIndex(item => item.routeName == routeName)
+
+          if(found != -1) {
+            this.routeInfo[found].stop.push({name: stopName, estimateTime: estimateTime})
+          } else {
+            this.routeInfo.push({
+              routeName: routeName,
+              stop: [{name: stopName, estimateTime: estimateTime}],
+            })
+          }
         }
       }
     },
@@ -93,21 +106,26 @@ export default {
 <style scoped>
 .title {
     text-align: center;
-  }
-  .dropdown {
-    text-align: center;
-  }
-  .container {
-    display: grid;
-    justify-content: space-around;
-    grid-template-columns: repeat( auto-fill, 295px);
-    margin:4vh 5vw 0;
-  }
-  .each {
-    border: 1px solid black;
-    height: 150px;
-    margin: 10px 0;
-    padding: 5px;
-    text-align: center;
-  }
+}
+.dropdown {
+  text-align: center;
+}
+.box {
+  margin:4vh 5vw 0;
+  border: 1px solid black;
+}
+.name {
+  margin: 0 7px;
+}
+.container {
+  display: grid;
+  justify-content: space-around;
+  grid-template-columns: repeat( auto-fill, 200px);
+}
+.each {
+  border: 1px solid black;
+  margin: 5px 0;
+  padding: 5px;
+  text-align: left;
+}
 </style>
